43a70e345b4a1c57aac93bf2d789d66a25765f64
randy-urriola <randy.urriolah@gmail.com>
2026-08-07 19:59:45 -0500

    Fin del repaso JS

--- js/32.js
+++ js/32.js
@@ -7,9 +7,25 @@ formulario.addEventListener('submit', e => {
   const nombre = document.querySelector('#nombre').value
   const password = document.querySelector('#password').value
 
+  // Prevenir nuevas alertas
+  const alertaPrevia = document.querySelector('.alerta')
+  alertaPrevia?.remove()
+
+  // nuevo elemento para colocar la alerta
+  const alerta = document.createElement('DIV')
+  alerta.classList.add('alerta', 'text-white', 'uppercase', 'text-sm', 'text-center', 'p-2', 'font-black')
+
   if (nombre === '' || password === '') {
-    console.log('Todos los campos son obligatorios...')
+    alerta.textContent = 'Todos los campos son obligatorios...';
+    alerta.classList.add('bg-red-500')
   } else {
-    console.log('Todos bien, iniciando sesion...');
+    alerta.textContent = 'Todos bien, iniciando sesion...';
+		alerta.classList.add('bg-green-500');
   }
+
+  formulario.appendChild(alerta)
+
+  setTimeout(() => {
+    alerta.remove()
+  }, 3000);
 })
